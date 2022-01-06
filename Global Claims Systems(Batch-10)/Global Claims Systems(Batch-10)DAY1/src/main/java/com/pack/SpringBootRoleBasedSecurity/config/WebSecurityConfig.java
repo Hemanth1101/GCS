84d2317ac9b5630a6496a19ee6be11533704c85d
Hemanth1101 <69164297+Hemanth1101@users.noreply.github.com>
2022-01-06 17:32:31 +0530

    Update WebSecurityConfig.java

--- Global Claims Systems(Batch-10)/Global Claims Systems(Batch-10)DAY1/src/main/java/com/pack/SpringBootRoleBasedSecurity/config/WebSecurityConfig.java
+++ Global Claims Systems(Batch-10)/Global Claims Systems(Batch-10)DAY1/src/main/java/com/pack/SpringBootRoleBasedSecurity/config/WebSecurityConfig.java
@@ -47,7 +47,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {{
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
+    }}
 }
